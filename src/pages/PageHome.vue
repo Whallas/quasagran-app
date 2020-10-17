9168a6a0250584c97f9e43efa14785c051b81e22
whallas <whallaspimnmetel@gmail.com>
2020-10-17 20:24:08 -0300

    Multiple posts in PageHome

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,42 +1,45 @@
 <template>
   <q-page class="constrain q-pa-md">
-
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-md-8">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post q-mb-md"
-          flat
-          bordered
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="" />
-              </q-avatar>
-            </q-item-section>
+        <div class="card-posts">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md"
+            flat
+            bordered
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" alt="" />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label>whallas09</q-item-label>
-              <q-item-label caption>
-                {{ post.location }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label>whallas09</q-item-label>
+                <q-item-label caption>
+                  {{ post.location }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
+            <q-separator />
 
-          <q-img :src="post.imageUrl" />
+            <q-img :src="post.imageUrl" />
 
-          <q-card-section>
-            <div class="">{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-          </q-card-section>
-        </q-card>
+            <q-card-section>
+              <div class="">{{ post.caption }}</div>
+              <div class="text-caption text-grey">
+                {{ post.date | niceDate }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
 
-      <div class="col-md-4">
+      <div class="col-4">
         <q-item class="fixed">
           <q-item-section avatar>
             <q-avatar size="48px">
@@ -53,7 +56,6 @@
         </q-item>
       </div>
     </div>
-
   </q-page>
 </template>
 
@@ -104,6 +106,10 @@ export default {
 </script>
 
 <style lang="sass">
+.card-posts
+  max-width: 600px
+  margin: 0 auto
+
 .card-post
   .q-img
     min-height: 200px
